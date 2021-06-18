493924cf3a1969f6edf6081a46ead8ccba3c556d
rosh-agahi <rosh.agahi@gmail.com>
2021-06-18 12:15:49 -0700

    form for new expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,11 +1,30 @@
 class ExpensesController < ApplicationController
 
   get '/expenses' do
-    erb :'/expenses/show.html'
+    erb :'/expenses/index.html'
   end
 
   get '/expenses/new' do
+    @categories = current_user.categories
     erb :'/expenses/new.html'
   end
+
+  post '/expenses/new' do
+    @expense = Expense.new
+    @expense.date = Date.today
+    @expense.amount = params[:amount]
+    @expense.vendor = params[:vendor]
+    @expense.description = params[:description]
+    @expense.category_id = params[:category_id]
+    @expense.user_id = current_user.id
+
+    if @expense.save
+      redirect "/expenses"
+    else
+      erb :'users/new.html'
+    end
+
+  end
+
 
 end
