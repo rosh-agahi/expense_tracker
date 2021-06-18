3a8882cb02c6c4861a5ab1ca14ef3f053e58e37b
rosh-agahi <rosh.agahi@gmail.com>
2021-06-18 10:44:45 -0700

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "exptracker-1234"
+
     set :public_folder, 'public'
     set :views, 'app/views'
   end
