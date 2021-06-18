fda99664a90703d758516db4cbae393d10f06881
rosh-agahi <rosh.agahi@gmail.com>
2021-06-18 08:42:22 -0700

    updated environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,20 @@
 ENV['SINATRA_ENV'] ||= "development"
+ENV['RAILS_ENV'] = ENV['SINATRA_ENV'] # maybe the migrator is looking for RAILS_ENV
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+set :environment, ENV['SINATRA_ENV'].to_sym
+
+database_config = {
+  :development =>  {
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  }
+}
+
+set :database, database_config
 
 require './app/controllers/application_controller'
+require 'rack-flash'
 require_all 'app'
