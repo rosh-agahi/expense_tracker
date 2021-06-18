e04526c604c5d5de0701095f266e963eac680a5d
rosh-agahi <rosh.agahi@gmail.com>
2021-06-18 10:50:42 -0700

    register new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,22 @@ class UsersController < ApplicationController
     erb :'users/new.html'
   end
 
+  post '/users' do
+    @user = User.new
+    @user.name = params[:name]
+    @user.username = params[:username]
+    @user.password = params[:password]
+
+    if @user.save
+      redirect "/login"
+    else
+      erb :'users/new.html'
+    end
+
+  end
+
   get '/login' do
     erb:'users/login.html'
-  end 
+  end
 
 end
