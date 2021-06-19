8a13f037d9610b9d1d08d1af0c2a6e180576459d
rosh-agahi <rosh.agahi@gmail.com>
2021-06-19 09:02:02 -0700

    categories new view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,26 @@
 class CategoriesController < ApplicationController
+
+  get '/categories' do
+    @categories = current_user.categories
+    erb :'/categories/index.html'
+  end
+
+  get '/categories/new' do
+    erb :'/categories/new.html'
+  end
+
+  post '/categories' do
+    @category = Category.new
+    @category.name = params[:name]
+    @category.user_id = current_user.id
+
+    if @category.save
+      redirect "/categories"
+    else
+      erb :'categories/new.html'
+    end
+
+  end
+
+
 end
