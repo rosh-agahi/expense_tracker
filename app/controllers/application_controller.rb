06ae1b6856cd9f82932b57b23c9b580c1ecfe4e4
rosh-agahi <rosh.agahi@gmail.com>
2021-06-23 17:12:53 -0700

    conditional nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,11 @@ class ApplicationController < Sinatra::Base
   helpers do
 
     def current_user
-      User.find_by_id(session[:user_id])
+      @user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+      !!current_user # hardens the true or false value return of current_user
     end
 
     def dollar_display(amount)
